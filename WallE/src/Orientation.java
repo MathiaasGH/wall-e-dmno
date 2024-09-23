9a42dfd7532626a3e0f5a1893ade1164689bea96
MathiaasGH <mathi@DESKTOP-ESR02KI>
2024-09-23 16:01:43 +0200

    création de l'orientation

---
+++ WallE/src/Orientation.java
@@ -0,0 +1,19 @@
+
+public class Orientation {
+
+	private String face; //valeur possible : devant, derrière, neutre. 
+	private String cote; //valeur possible : gauche, droite, neutre. 
+
+	/** Méthode qui renvoie la nouvelle position d’un objet
+	* @param int id l’identité de l’objet
+	* @return retourne la nouvelle position de l’objet
+	*/
+	public void actualiser(){}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
