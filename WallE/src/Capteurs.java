7c966420c7e1d3fc3907723436221de17d9c4ce8
carol <carol@Ordi-Carole>
2024-10-07 17:03:41 +0200

    Commit Capteur

--- WallE/src/Capteurs.java
+++ WallE/src/Capteurs.java
@@ -2,18 +2,40 @@ import java.util.Arrays;
 
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 
 public class Capteurs {
 
 	private EV3UltrasonicSensor vue;
-	private SampleProvider sp;
+	private EV3TouchSensor touche;
+	private EV3ColorSensor couleur;
+	private SampleProvider spVue;
+	private SampleProvider spTouche;
+	private SampleProvider spCouleur;
 	private Robot robot;
 	
 	public Capteurs(Robot r) {
 		robot = r;
+		touche = new EV3TouchSensor (SensorPort.S1);
 		vue = new EV3UltrasonicSensor(SensorPort.S4);
-		sp = vue.getDistanceMode();
+		couleur = new EV3ColorSensor(SensorPort.S2);
+		spVue = vue.getDistanceMode();
+		spTouche = touche.getTouchMode();
+		spCouleur = couleur.getColorIDMode();
+	}
+	
+	public float[] touche(float[] tab){
+		float[] newTab = Arrays.copyOf(tab, tab.length+1);
+		touche.fetchSample(newTab, newTab.length-1);
+		return newTab;
+	}
+	
+	public float[] capteCouleur(float[] tab) {
+		float[] newTab = Arrays.copyOf(tab, tab.length+1);
+		couleur.fetchSample(newTab, newTab.length-1);
+		return newTab; 
 	}
 	
 	public void fermeLesYeux() {
