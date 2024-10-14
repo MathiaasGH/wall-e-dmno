08ec1278e771e733f95b92bbd7db0c155252ef52
carol <carol@10.3.222.72>
2024-10-14 14:54:34 +0200

    updateOrientation dans Posititon

--- WallE/src/Position.java
+++ WallE/src/Position.java
@@ -1,7 +1,7 @@
 
 public class Position {
 	private int x, y; 
-	private int degres; 
+	private double degres; 
 	private char startSide; //b ou g pour blueSide ou greenSide
 	
 	
@@ -12,6 +12,22 @@ public class Position {
 		this.startSide=startSide; 
 	}
 	
-	
+	public void updateOrientation(double distanceTourne) {
+		double distanceAbsolue= Math.abs(distanceTourne);
+		if(distanceAbsolue>360) {
+			while(distanceAbsolue>360) {
+				distanceAbsolue=distanceAbsolue-360;
+			}
+		}
+		if (degres+distanceTourne>180) {
+			degres=-180+((Math.abs(degres+distanceTourne))-180);
+		}
+		else if (degres+distanceTourne<-180) {
+			degres=180-((Math.abs(degres+distanceTourne))-180);
+		}
+		else {
+			degres=degres+distanceTourne;
+		}
+	}
 	
 }
