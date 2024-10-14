3a6d852dcadb86ea6fcd7ae3f682eda4cb5518ea
CaroleLeCrac <165332354+CaroleLeCrac@users.noreply.github.com>
2024-10-14 13:37:43 +0200

    Add files via upload

---
+++ WallE/src/Position.java
@@ -0,0 +1,17 @@
+
+public class Position {
+	private int x, y; 
+	private int degres;
+	
+	public Position() {
+		this(0,0);
+	}
+	
+	public Position(int x, int y) {
+		this.x=x; 
+		this.y=y;
+		degres = 0;
+	}
+	
+	
+}
