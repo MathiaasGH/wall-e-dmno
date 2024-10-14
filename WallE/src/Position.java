efbc5f64ece18a3336176ef1d04ca1f50ad787cf
carol <carol@10.3.222.72>
2024-10-14 13:39:27 +0200

    Position avec startSide

--- WallE/src/Position.java
+++ WallE/src/Position.java
@@ -1,17 +1,17 @@
 
 public class Position {
 	private int x, y; 
-	private int degres;
+	private int degres; 
+	private char startSide; //b ou g pour blueSide ou greenSide
 	
-	public Position() {
-		this(0,0);
-	}
 	
-	public Position(int x, int y) {
+	public Position(int x, int y, char startSide) {
 		this.x=x; 
 		this.y=y;
 		degres = 0;
+		this.startSide=startSide; 
 	}
+	
 	
 	
 }
